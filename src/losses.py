011df6ce0d5ab18ac24a10e712d85b4dc4024b1e
Гончаров Павел Владимирович <pgoncharov@intervale.ru>
2019-07-12 20:41:45 +0300

    focal loss added

--- src/losses.py
+++ src/losses.py
@@ -37,14 +37,52 @@ class FakesLoss(torch.nn.Module):
 
 
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=0, alpha=None, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        if isinstance(alpha,(float,int)): self.alpha = torch.Tensor([alpha,1-alpha])
+        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim()>2:
+            input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
+            input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
+            input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
+        target = target.view(-1,1)
+
+        logpt = F.log_softmax(input)
+        logpt = logpt.gather(1,target)
+        logpt = logpt.view(-1)
+        pt = Variable(logpt.data.exp())
+
+        if self.alpha is not None:
+            if self.alpha.type()!=input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0,target.data.view(-1))
+            logpt = logpt * Variable(at)
+
+        loss = -1 * (1-pt)**self.gamma * logpt
+        if self.size_average: return loss.mean()
+        else: return loss.sum()
+
+
+
 class LootLoss(torch.nn.Module):
     def __init__(self):
         super(LootLoss, self).__init__()
-        self.probs_loss = FakesLoss()
+        self.probs_loss = FocalLoss(gamma=2, alpha=0.9)
         self.shifts_loss = ShiftsMSELoss()
 
     def forward(self, inputs, target):
-        probs_loss_value = self.probs_loss(inputs, target)
+        probs_loss_value = self.probs_loss(inputs[:, 0], target[:, 0])
         shifts_loss_value = self.shifts_loss(inputs, target)
         loss = probs_loss_value + shifts_loss_value
         return loss
