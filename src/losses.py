30d748e900ccf06f4a7dbc60ba4523015d0e91e1
Гончаров Павел Владимирович <pgoncharov@intervale.ru>
2019-09-25 23:34:15 +0300

    add dice loss

--- src/losses.py
+++ src/losses.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class ShiftsMSELoss(torch.nn.Module):
+class ShiftsMSELoss(nn.Module):
     def __init__(self):
         super(ShiftsMSELoss, self).__init__()
     
@@ -23,10 +23,10 @@ class ShiftsMSELoss(torch.nn.Module):
         return loss
 
 
-class FakesLoss(torch.nn.Module):
+class FakesLoss(nn.Module):
     def __init__(self):
         super(FakesLoss, self).__init__()
-        self.bce_loss = torch.nn.BCELoss()
+        self.bce_loss = nn.BCELoss()
 
     def forward(self, inputs, target):
         true_mask = target[:, 0]
@@ -61,14 +61,41 @@ class FocalLoss(nn.Module):
             return F_loss
 
 
-class LootLoss(torch.nn.Module):
-    def __init__(self):
+class DiceLoss(nn.Module):
+    def forward(self, inputs, targets):
+        smooth = 1.
+        iflat = inputs.contiguous().view(-1)
+        tflat = targets.contiguous().view(-1)
+        intersection = (iflat * tflat).sum()
+        A_sum = torch.sum(iflat * iflat)
+        B_sum = torch.sum(tflat * tflat)
+        return 1 - ((2. * intersection + smooth) / (A_sum + B_sum + smooth))
+
+
+class LootLoss(nn.Module):
+    def __init__(self, lambda1, lambda2):
         super(LootLoss, self).__init__()
-        self.probs_loss = FocalLoss(gamma=2, alpha=0.9)
+        
+        if lambda1 < 0:
+            raise ValueError("lambda1 %.2f < 0! Value greater than zero is required" % lambda1)
+
+        if lambda2 < 0:
+            raise ValueError("lambda2 %.2f < 0! Value greater than zero is required" % lambda2)
+        
+        self.lambda1 = lambda1
+        self.lambda2 = lambda2
+        self.dice_loss = DiceLoss()
+        self.binary_ce = nn.BCELoss()
         self.shifts_loss = ShiftsMSELoss()
 
+    def probs_loss(self, pred, target):
+        """Sum of Dice loss and Binary Cross-entropy"""
+        probs_loss_value = self.dice_loss(pred, target)
+        probs_loss_value += self.binary_ce(pred, target)
+        return probs_loss_value
+
     def forward(self, inputs, target):
-        probs_loss_value = self.probs_loss(inputs[:, 0], target[:, 0])
-        shifts_loss_value = self.shifts_loss(inputs, target)
-        loss = probs_loss_value + shifts_loss_value
-        return loss
+        probs_loss_value = self.probs_loss(inputs[:, 0], target[:, 0]) * self.lambda1
+        shifts_loss_value = self.shifts_loss(inputs, target) * self.lambda2
+        return probs_loss_value + shifts_loss_value
+    
